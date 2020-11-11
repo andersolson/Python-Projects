96233643b83df50dc0ad6012c12a0098e4c43fa6
andersolson <anders.olson7@gmail.com>
2020-11-10 17:42:57 -0700

    Update AppleBirthday.py
    message stuff

--- AppleBirthday/AppleBirthday.py
+++ AppleBirthday/AppleBirthday.py
@@ -4,5 +4,5 @@ adoptiondate = datetime.date(2016, 2, 14)
 
 birthDate    = adoptiondate - datetime.timedelta(547) #Apple was already 1.5 years old or 547 days old
 
-print "Apple's adoption date is {}".format(adoptiondate)
-print "Apple's birthday is {}".format(birthDate)
+print("Apple's adoption date is {}".format(adoptiondate))
+print("Apple's birthday is {}".format(birthDate))
