2c26659c86e52055993ecfe8048d2f458b8ef54f
aolson-c3 <151224694+aolson-c3@users.noreply.github.com>
2024-04-12 11:29:26 -0600

    find center point
    find center point

--- chaos-magick/plotTeslaVortex.py
+++ chaos-magick/plotTeslaVortex.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = [9, 9]
 plt.rcParams["figure.autolayout"] = True
 
-x = [9, 13, 15, 14, 11, 7, 4, 3, 5]
-y = [15, 14, 10, 6, 3, 3, 6, 10, 14]
+x = [9, 13, 15, 14, 11, 7, 4, 3, 5, 9]
+y = [15, 14, 10, 6, 3, 3, 6, 10, 14, 9]
+
+xCen = [p for p in x]
+yCen = [p for p in y]
+centroid = (sum(xCen) / len(x), sum(yCen) / len(y))
+print(centroid)
 
 plt.plot(x, y,'o')
 plt.axis([0, 18, 0, 18])
