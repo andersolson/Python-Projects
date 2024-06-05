0282949084260a840adf337d2770f7359fbfbeb4
aolson-c3 <151224694+aolson-c3@users.noreply.github.com>
2024-06-05 10:48:49 -0600

    Update UniqueID.py
    Reaady

--- random/UniqueID.py
+++ random/UniqueID.py
@@ -1,15 +1,14 @@
 count = 0
+startNumber = 40220
 def uniqueID():
-    global count
 
-    startNumber = 40220
+    global count,startNumber
 
     while count <= 200:
 
         count += 1
-
         startNumber += 1
 
         return startNumber
 
-genID()
+uniqueID()
