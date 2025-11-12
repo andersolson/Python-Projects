4d03f37c28f59ef0f8c67ad2d8ca72891c65bb20
aolson-c3 <151224694+aolson-c3@users.noreply.github.com>
2025-11-12 10:23:46 -0700

    Create FieldValueCompare.py
    first commit

---
+++ FieldValueCompare.py
@@ -0,0 +1,67 @@
+'''****************************************************************
+FieldValueCompare.py
+Author(s): Anders Olson
+Usage: Run script using python IDE or similar
+Description:
+        Script traverses ID field for 2 shapefiles and compares the
+        list of IDs to see what is missing.
+****************************************************************'''
+
+import arcpy
+from collections import counter
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#================================#
+# Configure logger
+#================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+def outputMessage(msg):
+    print(msg)
+    arcpy.AddMessage(msg)
+
+def outputError(msg):
+    print(msg)
+    arcpy.AddError(msg)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#================================#
+# Define environment and messaging
+#================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# Set the overwriteOutput ON
+arcpy.gp.overwriteOutput = True
+
+# Set workspace to be in memory for faster run time
+arcpy.env.workspace = "in_memory"
+
+outputMessage("Workspace is: {}".format(arcpy.env.workspace))
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#================================#
+# Define variables
+#================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+#Input shapefile or dbf with id field to count
+inData = r"U:\Somedirectory\yourshapefile.shp"
+
+# Empty list for storing all unique IDs
+uniqueIDs = []
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#================================#
+# Call Functions
+#================================#
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+# Populate a list of all IDs found in the desired ID field
+for row in arcpy.da.SearchCursor(inData, ['<FIELD-NAME>']):
+        uniqueIDs.append(row[0])
+
+# Print a list of all ids and no repeating ids i.e. 'keys'
+print(Counter(uniqueIDs).keys())
+
+# Print a count for occurrence of each id found in original list i.e. 'values'
+print(Counter(uniqueIDs).values())
