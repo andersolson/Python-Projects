6103f5910f45a04531e47ba527f320c7c2ca7bc7
aolson-c3 <aolson@c3gov.com>
2026-05-05 14:04:17 -0600

    new documentation
    new documentation

--- chaos-magick/documentation/plotTrollrunGrid.py
+++ chaos-magick/documentation/plotTrollrunGrid.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = [9, 9]
 plt.rcParams["figure.autolayout"] = True
 
-x = [9, 13, 15, 14, 11, 7, 4, 3, 5]
-y = [15, 14, 10, 6, 3, 3, 6, 10, 14]
+x = [1, 0, -1, 1, 0, -1, 1, 0, -1, 1, -1, 1, -1]
+y = [2, 1, 2, 1, 0, 1, 0, -1, 0, -1, -1, -2, -2]
 
 xCen = [p for p in x]
 yCen = [p for p in y]
@@ -12,7 +12,7 @@ centroid = (sum(xCen) / len(x), sum(yCen) / len(y))
 print(centroid)
 
 plt.plot(x, y,'o')
-plt.axis([0, 4, 0, 4])
+plt.axis([-3, 3, -3, 3])
 
 for i, j in zip(x, y):
    plt.text(i, j+0.5, '({}, {})'.format(i, j))
