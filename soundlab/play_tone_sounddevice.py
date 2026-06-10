a643d0a0f13f9e91c2a0f0977b7ea8353641b037
aolson-c3 <aolson@c3gov.com>
2026-06-10 11:07:40 -0600

    testing a 440hz tone
    testing a 440hz tone

---
+++ soundlab/play_tone_sounddevice.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sounddevice as sd
+
+def play_tone(frequency, duration, volume=0.25):
+    samplerate = 44100
+    t = np.linspace(0, duration, int(duration * samplerate), False)
+    tone = np.sin(2 * np.pi * frequency * t) * volume
+    sd.play(tone, samplerate)
+    sd.wait()
+
+play_tone(frequency=440, duration=1)
+
