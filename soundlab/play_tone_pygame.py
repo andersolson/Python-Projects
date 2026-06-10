6ba7b1284ba618d9f93951e28d638d44cd7cd110
aolson-c3 <aolson@c3gov.com>
2026-06-10 16:44:53 -0600

    error is fixed audio needed to be in stereo
    error is fixed audio needed to be in stereo

--- soundlab/play_tone_pygame.py
+++ soundlab/play_tone_pygame.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pygame
 
-pygame.mixer.init(frequency=44100, size=-16, channels=1)
+pygame.mixer.init(frequency=48000, size=-16, channels=1)
 
-def play_tone(freq, duration=1.0, volume=0.5):
-    sample_rate = 44100
+def play_tone(freq, duration=5.0, volume=0.5):
+    sample_rate = 48000
     t = np.linspace(0, duration, int(sample_rate * duration), False)
     tone = np.sin(2 * np.pi * freq * t)
+
+    # Convert mono -> stereo (duplicate the channel)
     audio = (tone * 32767).astype(np.int16)
-    sound = pygame.sndarray.make_sound(audio)
+    audio_stereo = np.column_stack((audio, audio))
+
+    sound = pygame.sndarray.make_sound(audio_stereo)
     sound.set_volume(volume)
     sound.play()
     pygame.time.delay(int(duration * 1000))
