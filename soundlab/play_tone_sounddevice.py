aa8346f16a7b6151cf419243a6099f2d9effda5e
aolson-c3 <aolson@c3gov.com>
2026-06-10 11:16:52 -0600

    change the samples per second
    change the samples per second

--- soundlab/play_tone_sounddevice.py
+++ soundlab/play_tone_sounddevice.py
@@ -2,7 +2,7 @@ import numpy as np
 import sounddevice as sd
 
 def play_tone(frequency, duration, volume=0.25):
-    samplerate = 44100
+    samplerate = 48000 #44100 = 44.1kHz standard audio cd quality
     t = np.linspace(0, duration, int(duration * samplerate), False)
     tone = np.sin(2 * np.pi * frequency * t) * volume
     sd.play(tone, samplerate)
