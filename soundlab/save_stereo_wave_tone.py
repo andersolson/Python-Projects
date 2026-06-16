b7975aece95f78b32d86b29c17b370dd220a8be1
aolson-c3 <aolson@c3gov.com>
2026-06-16 09:00:07 -0600

    testing with new frequencies
    testing with new frequencies

--- soundlab/save_stereo_wave_tone.py
+++ soundlab/save_stereo_wave_tone.py
@@ -24,4 +24,4 @@ def save_stereo_wav(filename, freq_left, freq_right, duration=1.0, volume=0.5):
         f.writeframes(stereo_audio.tobytes())
 
 # Example usage:
-save_stereo_wav(r"C:\Users\is_olson\Music\delta_5min_50-54.wav", 50, 54, duration=300.0)
+save_stereo_wav(r"C:\Users\is_olson\Music\gamma_5min_50-150.wav", 50, 150, duration=300.0)
